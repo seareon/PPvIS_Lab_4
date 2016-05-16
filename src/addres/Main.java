6996ce79b6fd0a41cd07b0f8ee135f615bfd6516
Mike Ro <seareon1587.mr@gmail.com>
2016-05-16 23:57:02 +0300

    v 2.1

---
+++ src/addres/Main.java
@@ -0,0 +1,27 @@
+package addres;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	public void start(Stage stage) throws Exception {
+		try {
+		Parent root = FXMLLoader.load(getClass().getResource("test.fxml")); 
+		Scene scene = new Scene(root); 
+		stage.setTitle("ScrollPane Example");  
+		stage.setScene(scene); 
+		stage.show(); 
+		} catch(IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
